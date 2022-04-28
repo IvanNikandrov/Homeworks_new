a06677ae1323048f66b9088448d7e6e9558cbcf7
IvanNikandrov <ivannikandrov1@gmail.com>
2022-04-28 14:27:17 +0300

    commit

--- homework_03/testproject/users/dependencies.py
+++ homework_03/testproject/users/dependencies.py
@@ -2,7 +2,6 @@ from fastapi import Header, HTTPException
 from starlette.status import HTTP_401_UNAUTHORIZED
 
 from . import crud
-from .schemas import User
 
 
 def get_user_by_auth_token(token: str = Header(..., description='user auth token')):
