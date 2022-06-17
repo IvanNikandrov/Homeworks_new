27dc62e7d5307eb5bfb7b95abef557b159d8a10e
ivanNikandrov <ivannikandrov1@gmail.com>
2022-06-17 15:45:33 +0300

    correct route

--- homework_05/app.py
+++ homework_05/app.py
@@ -23,7 +23,7 @@ def index():
     return render_template('index.html', title='Главная', menu=menu)
 
 
-@app.route('/about')
+@app.route('/about/')
 def about():
     return render_template('about.html', title='О нас', menu=menu)
 
